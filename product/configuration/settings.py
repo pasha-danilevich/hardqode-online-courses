4e67c1641e80ec798a269db614079cb8f375e8dc
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-18 00:17:48 +0300

    Update settings.py

--- product/configuration/settings.py
+++ product/configuration/settings.py
@@ -150,3 +150,20 @@ DJOSER = {
     },
     "LOGIN_FIELD": "email",
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
+# LOGGING = {}
